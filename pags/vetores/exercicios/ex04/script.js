41c55887123fea91c1cd57caff7be4c46decb7a1
GUILHERME-GARCIATECH <guilherme.garciatech@gmail.com>
2026-04-08 14:13:42 -0400

    Delete pags/vetores directory

--- pags/vetores/exercicios/ex04/script.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-    let res = document.querySelector('div#res');
-    res.innerHTML = '';
-
-    for(let i = 2; i < 50; i+=2){
-        let v = i;
-        res.innerHTML += `<p>${i}</p>`
-    }
-}
